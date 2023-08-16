9991e6c44f76c0408639ef367bddb5dee4b77a79
mishapyt74 <139381915+mishapyt74@users.noreply.github.com>
2023-08-16 15:09:16 +0300

    hw

--- clean_folder/clean_folder/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-def clean():
-    some_var = sys.argv[1]
-    print(some_var)
-
-if __name__ == "__main__":
-    clean()
---
+++ clean_folder/clean_folder/main.py
@@ -0,0 +1,8 @@
+import sys
+
+def clean():
+    some_var = sys.argv[1]
+    print(some_var)
+
+if __name__ == "__main__":
+    clean()
